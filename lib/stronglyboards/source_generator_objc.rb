8d0374628d8fcf0e3c377d7cfd47287c3ac1e625
Steve Wilford <steve@offtopic.io>
2015-09-27 11:42:51 +0100

    Generate the forward declarations before any classes. Consolidated output into the finalize function for better control over the overall file structure.

--- lib/stronglyboards/source_generator_objc.rb
+++ lib/stronglyboards/source_generator_objc.rb
@@ -11,20 +11,11 @@ module Stronglyboards
       @header_file = File.open(@header_file_path, 'w+')
       @implementation_file = File.open(@implementation_file_path, 'w+')
 
-      @header_file.write("@import UIKit;\n\n")
-      @implementation_file.write("#import \"#{File.basename(@header_file_path)}\"\n\n")
-
-      puts "Header:         #{@header_file_path}"
-      puts "Implementation: #{@implementation_file_path}"
-
       @storyboards = Array.new
     end
 
-    # Generates source code for components of the provided storyboard
     def process(storyboard)
-      # Store this storyboard for further processing later
       @storyboards.push(storyboard)
-      createStoryboardClass(storyboard)
     end
 
     # Generate the class for the provided storyboard
@@ -33,13 +24,10 @@ module Stronglyboards
       class_name = storyboard.class_name(@prefix)
       puts "Processing storyboard class #{class_name}."
 
-      view_controller_classes = Array.new(storyboard.view_controllers.length)
       interface = Array.new(1, "@interface #{class_name} : UIStoryboard")
       implementation = Array.new(1, "@implementation #{class_name}")
 
       storyboard.view_controllers.each do |vc|
-        view_controller_classes.push("@class #{vc.class_name};")
-
         if vc.initial_view_controller?
           # Provide an overridden declaration of the UIStoryboard -instantiateInitialViewController method
           interface.push("- (#{vc.class_name} *)instantiateInitialViewController;")
@@ -58,12 +46,10 @@ module Stronglyboards
       implementation.push('@end')
 
       # Convert to string
-      view_controller_classes = view_controller_classes.join("\n")
       interface = interface.join("\n")
       implementation = implementation.join("\n")
 
       # Output to file
-      @header_file.write(view_controller_classes)
       @header_file.write(interface)
       @implementation_file.write(implementation)
 
@@ -72,6 +58,28 @@ module Stronglyboards
     # Finalizes processing
     public
     def finalize
+
+      # Generate framework and header imports
+      @header_file.write("@import UIKit;\n\n")
+      @implementation_file.write("#import \"#{File.basename(@header_file_path)}\"\n\n")
+
+      puts "Header:         #{@header_file_path}"
+      puts "Implementation: #{@implementation_file_path}"
+
+      # Gather a set of view controller classes from all storyboards
+      view_controller_classes = @storyboards.collect { |storyboard|
+        storyboard.view_controllers.collect { |vc| vc.class_name }
+      }.flatten.uniq
+
+      # Generate forward declaration of view controller classes
+      view_controller_classes.each do |class_name|
+        @header_file.write("@class #{class_name};\n")
+      end
+
+      # Generate classes for each storyboard
+      @storyboards.each { |storyboard| createStoryboardClass(storyboard) }
+
+      # Generate the storyboard category
       createStoryboardCategory
     end
 

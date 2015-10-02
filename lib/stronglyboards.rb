4796b8c12ea1127eef4074c74e217d7d9f0ee32e
Steve Wilford <steve@offtopic.io>
2015-10-02 15:09:52 +0100

    Added uninstall command. Closes issue #4 Refactored lock file loading into it's own method. Refactored target targeting (lol) into it's own method.

--- lib/stronglyboards.rb
+++ lib/stronglyboards.rb
@@ -3,16 +3,19 @@ require 'optparse'
 require 'thor'
 require 'yaml'
 
-require 'stronglyboards/version'
-require 'stronglyboards/storyboard'
-require 'stronglyboards/source_generator_objc'
-require 'stronglyboards/source_generator_swift'
+require_relative 'stronglyboards/version'
+require_relative 'stronglyboards/storyboard'
+require_relative 'stronglyboards/source_generator_objc'
+require_relative 'stronglyboards/source_generator_swift'
 
 module Stronglyboards
 
   class Stronglyboards < Thor
 
     LOCK_FILE_NAME = 'Stronglyboards.lock'
+    BUILD_SCRIPT_NAME = 'Update Stronglyboards'
+
+    # ---- Begin external interface ----
 
     desc 'install PROJECT', 'Installs Stronglyboards into your .xcodeproj file'
     option :output, :desc => 'Path to the output file'
@@ -25,7 +28,7 @@ module Stronglyboards
         return
       end
 
-      puts "Installing into #{project_file}"
+      puts "Installing Stronglyboards into #{project_file}"
 
       # Open the existing Xcode project
       project = Xcodeproj::Project.open(project_file)
@@ -46,37 +49,111 @@ module Stronglyboards
 
     desc 'update', 'Updates the generated source code for the project'
     def update(project_name)
-      if !File.exists?(LOCK_FILE_NAME)
-        puts 'Stronglyboards must first be installed using the install command.'
-      else
-        puts 'Updating Stronglyboards...'
+      configuration = load_lock_file
 
-        # Load the lock file containing configuration
-        lock_file = File.open(LOCK_FILE_NAME, 'r')
-        options = YAML::load(lock_file)
+      puts 'Updating Stronglyboards...'
 
-        # Open the Xcode project
-        project = Xcodeproj::Project.open("#{project_name}.xcodeproj")
+      # Open the Xcode project
+      project = Xcodeproj::Project.open("#{project_name}.xcodeproj")
 
-        process(project, options)
-      end
+      process(project, configuration)
     end
+
+    desc 'uninstall PROJECT', 'Uninstalls Stronglyboards from the specified .xcodeproj file.'
+    def uninstall(project_file)
+      configuration = load_lock_file
+
+      base_output_file = configuration[:output]
+      language = configuration[:language]
+      prefix = configuration[:prefix]
+
+      puts "Uninstalling Stronglyboards from #{project_file}"
+
+      files_to_delete = Array.new
+
+      # Open the Xcode project
+      project = Xcodeproj::Project.open(project_file)
+      project_root = project.path.dirname
+
+      # Gather the targets that we're interested in
+      targets = interesting_targets(project)
+
+      targets.each do |target|
+        # Find and delete the build script phase for this target
+        target.build_phases.select { |phase|
+          phase.is_a? Xcodeproj::Project::PBXShellScriptBuildPhase and phase.name == BUILD_SCRIPT_NAME
+        }.each { |phase|
+          target.build_phases.delete(phase)
+        }
+
+        # Get a source generator for this target
+        output_file = base_output_file_for_target(base_output_file, target, prefix)
+        source_generator = source_generator(language, prefix, output_file)
+
+        # Gather the files that would have been generated for this target.
+        # Bare in mind that this target may have been created since the
+        # last time the install or update command ran, so there may be
+        # files in this list that don't actually exist.
+        files_to_delete << source_generator.output_files
+      end
+
+      # Expand the paths of the files to be deleted to be absolute
+      files_to_delete.flatten!.uniq!
+      paths_to_delete = files_to_delete.collect do |file|
+        project_root + file.file.path
+      end
+
+      # Look through each target to see if this file is a member,
+      # removing it from the source build phase if necessary.
+      # TODO: There's got to be a better way, question asked.
+      # http://stackoverflow.com/questions/32908231/how-to-get-the-targets-for-a-pbxfilereference-in-xcodeproj
+      targets.each do |target|
+        target.source_build_phase.files.each do |build_file|
+          full_path = build_file.file_ref.real_path
+          if paths_to_delete.include?(full_path)
+
+            # Need to get a reference to the underlying file reference
+            # as it will be removed when the build file is removed
+            # from the target's build phase.
+            file = build_file.file_ref
+
+            # Remove the file from the build phase, project, and file system
+            target.source_build_phase.remove_build_file(build_file)
+            file.remove_from_project
+            File.delete(full_path)
+          end
+        end
+      end
+
+      # Iterate through the files to delete to get rid of any non-source files
+      files_to_delete.each do |file|
+        unless file.is_source
+          full_path = File.realpath(file.file)
+          file_ref = project.reference_for_path(full_path)
+          file_ref.remove_from_project
+          File.delete(full_path)
+        end
+      end
+
+      project.save
+
+      # Finally delete the lock file
+      File.delete(LOCK_FILE_NAME)
+
+    end
+
+    # ---- End external interface ----
 
     private
     def process(project, options)
-      output_file = options[:output]
+      base_output_file = options[:output]
       language = options[:language]
       prefix = options[:prefix]
 
-      project.native_targets
-          .select { |target| target.product_type == 'com.apple.product-type.application' }
-          .each do |target|
+      interesting_targets(project).each do |target|
 
         # Provide a default output filename
-        if output_file == nil
-          output_file = prefix + 'Stronglyboards'
-        end
-        output_file += "_#{target.name}"
+        output_file = base_output_file_for_target(base_output_file, target, prefix)
 
         # Instantiate a source generator appropriate for the selected language
         source_generator = source_generator(language, prefix, output_file)
@@ -116,6 +193,19 @@ module Stronglyboards
     end
 
     private
+    def interesting_targets(project)
+      project.native_targets.select { |target| target.product_type == 'com.apple.product-type.application' }
+    end
+
+    private
+    def base_output_file_for_target(base_file, target, prefix)
+      if base_file == nil
+        base_file = prefix + 'Stronglyboards'
+      end
+      base_file + "_#{target.name}"
+    end
+
+    private
     def add_files_to_target(project, target, output_files)
       puts "Adding files to target \"#{target}\""
 
@@ -134,7 +224,7 @@ module Stronglyboards
       puts 'Adding build script'
 
       phase = project.new(Xcodeproj::Project::Object::PBXShellScriptBuildPhase)
-      phase.name = 'Update Stronglyboards'
+      phase.name = BUILD_SCRIPT_NAME
       phase.shell_script = 'stronglyboards update ${PROJECT_NAME}'
       target.build_phases.insert(0, phase)
     end
@@ -154,6 +244,18 @@ module Stronglyboards
     end
 
     private
+    def load_lock_file
+      if !File.exists?(LOCK_FILE_NAME)
+        puts 'Stronglyboards must first be installed using the install command.'
+        exit
+      else
+        # Load the lock file containing configuration
+        lock_file = File.open(LOCK_FILE_NAME, 'r')
+        YAML::load(lock_file)
+      end
+    end
+
+    private
     def source_generator(language, prefix, output_file)
       case language
          when 'objc'
@@ -167,5 +269,7 @@ module Stronglyboards
     end
 
   end
+
+  Stronglyboards.start(ARGV)
 
 end
